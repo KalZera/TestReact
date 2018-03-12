56070a6918a46c426f1ca2e0a0ba1c956f777661
KalZera <sk9.22alex@gmail.com>
2018-03-12 13:34:25 -0300

    commit create

---
+++ react-tutorial/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class App extends React.Component{
+    render() {
+        return (
+            <div>
+            {/* <div class="collection">
+                <a href="" class="colletion-header black-text"> <h4>Eventos</h4></a>
+                <a href="https://www.facebook.com/events/603918776615154/" class="collection-item">
+                Kaos Multicultural</a>
+                <a href="#!" class="collection-item">evento 2</a>
+                <a href="#!" class="collection-item">evento 3</a>
+                <a href="#!" class="collection-item">evento 4</a>
+            </div> */}
+            
+        </div>
+        );
+    }
+}
